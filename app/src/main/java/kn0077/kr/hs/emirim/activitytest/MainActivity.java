691bdbda7d4260086d8b8ae2f843bf5967d3ff2e
Kim kyuna <kkn00077@gmail.com>
2017-04-13 19:59:25 +0900

    #2 make activity MainActivity.xml and SecondActivity.xml

--- app/src/main/java/kn0077/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/kn0077/kr/hs/emirim/activitytest/MainActivity.java
@@ -19,6 +19,8 @@ getApplicationContext(); 는 현재 표시되어있는 액티비티(화면) 클�
 그래서 즉 지금 화면에 표시가 될 MainActivity를 참조해 반환하겠다는 말과 똑같다. 즉슨 MainActivity.this 라고 표현할 수 있다.
 그러나 getApplicationContext(); != MainActivity.this 이다.
 
+getApplicationContext();를 인자값으로 보내주는 것은 현재 액티비티 위에 덮어씌우듯 다른 액티비티를 쌓아가는 방식이기 때문에 이것을 위해서 인자값으로 넣어주어야한다.
+
 왜 MainActivity가 아니라 MainActivity.this로 표현해주냐면, MainActivity는 순전히 클래스명이며, 클래스명은 new를 써서 사용자 지정 변수를 지정할 때만 쓰기 때문이다.
 
  */
